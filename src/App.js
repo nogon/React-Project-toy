19b65697bd68f99b8a39c764daba3dcd48d06dc8
kje <jekim@arcahr.co.kr>
2021-12-21 15:48:54 +0900

    .map() 사용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-      <header className="grey-bg">
-        <img src={logo} lat="logo" />
-        <h2>Let's go!</h2>
-      </header>
-  );
+const customers = [
+  {
+    'id' : 1,
+    'img' : 'https://placeimg.com/64/64/1',
+    'name' : '홍길동',
+    'birth' : '920523',
+    'gender' : '남자',
+    'job' : '대학생'
+  }, {
+    'id' : 2,
+    'img' : 'https://placeimg.com/64/64/2',
+    'name' : '이순신',
+    'birth' : '820523',
+    'gender' : '남자',
+    'job' : '직장인'
+  }, {
+    'id' : 3,
+    'img' : 'https://placeimg.com/64/64/3',
+    'name' : '강감찬',
+    'birth' : '720523',
+    'gender' : '남자',
+    'job' : '프리랜서'
+  },
+]
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        { customers.map(c => {
+            return(
+              <Customer key={c.id}
+                id={c.id}
+                img={c.img}
+                name={c.name}
+                birth={c.birth}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          }) }
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Customer.js
@@ -0,0 +1,41 @@
+// react lib 불러오기
+import React from 'react';
+
+// Customer Class 정의하기. React에 Component 형태로 작성된 Class
+class Customer extends React.Component {
+    render() {
+        const name = this.props.name;
+        return  (
+            <div>
+                <CustomerProfile id={this.props.id} img={this.props.img} name={this.props.name} />
+                <CustomerInfo birth={this.props.birth} gender={this.props.gender} job={this.props.job} />
+            </div>
+        )
+    }
+}
+
+class CustomerProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.img} alt="profile" />
+                <h2>{this.props.id} , {this.props.name}</h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.birth}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
+        )
+    }
+}
+
+// Customer 내보내기
+export default Customer;
