9f9653e0136f98e8b7f00ff8f5afc716d3b3ab7f
kje <jekim@arcahr.co.kr>
2021-12-22 11:56:47 +0900

    Add Node.js Express Server Module - bodyParser를 주석 처리해야만 화면이 뜨는 이유는 뭘까? 버전차이?

---
+++ server.js
@@ -0,0 +1,18 @@
+// server program
+
+const express = require('express');
+//const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//app.use(bodyParser.json);
+//app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api/hello', (req, res) => {
+    res.send({message: 'Hello Express!'});
+});
+
+app.listen(port, (err) => {
+    if(err) return console.log(err);
+    console.log(`Listening on port ${port}`)
+});
