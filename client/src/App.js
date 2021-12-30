a7245edffa1ccc4ab4b5df2a20db0ae61f82cd20
kje <jekim@arcahr.co.kr>
2021-12-30 12:52:36 +0900

    Add App Bar, Font

--- client/src/App.js
+++ client/src/App.js
@@ -16,19 +16,88 @@ import { CircularProgress } from '@material-ui/core';
 
 import CustomerAdd from './components/CustomerAdd';
 
+// App bar
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { alpha, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing(3),
-    overflowX: 'auto'
+    minWidth: 1080
   },
-  table: {
-    minWidth:1080
+  paper: {
+    marginLeft: 20,
+    marginRight: 20
   },
   progress: {
     margin: theme.spacing(2)
+  },
+  tableHead: {
+    fontSize: '1.0rem'
+  },
+  menu: {
+    marginTop: 15,
+    marginBottom: 15,
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
   }
-})
+});
+
 
 class App extends React.Component {
 
@@ -89,19 +158,55 @@ class App extends React.Component {
 
   render() {
     const { classes } = this.props;
+    const cellList = ["번호","사진","이름","생년월일","성별","직업","설정"];
     return (
-      <div>
-        <Paper className={classes.root}>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" noWrap>
+              Customer Manage System
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="검색"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+        <div className={classes.menu}>
+        {/* props값으로 stateRefresh를 설정. 함수 자체를 props 형태로 보내주는 것.  */}
+        <CustomerAdd stateRefresh={this.stateRefresh} />
+        </div>
+        <Paper className={classes.paper}>
           <Table className={classes.table}>
             <TableHead>
               <TableRow>
-                <TableCell>번호</TableCell>
+                {/* <TableCell>번호</TableCell>
                 <TableCell>사진</TableCell>
                 <TableCell>이름</TableCell>
                 <TableCell>생년월일</TableCell>
                 <TableCell>성별</TableCell>
                 <TableCell>직업</TableCell>
-                <TableCell>설정</TableCell>
+                <TableCell>설정</TableCell> */}
+                {cellList.map(c => {
+                  return ( <TableCell className={classes.tableHead}>{c}</TableCell>)
+                })}
               </TableRow>
             </TableHead>
 
@@ -131,8 +236,7 @@ class App extends React.Component {
             </TableBody>
           </Table>
         </Paper>
-        {/* props값으로 stateRefresh를 설정. 함수 자체를 props 형태로 보내주는 것.  */}
-        <CustomerAdd stateRefresh={this.stateRefresh} />
+        
       </div>
     )
   }
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./font/font.css";
 
+import { MuiThemeProvider } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"GowunDodum-Regular", "Noto Sans KR", serif'
+  }
+})
+
+// App을 MuiThemeProvider로 감싸고 위에서 명시한 theme을 적용할 수 있도록 한다.
+// App 전체에 설정한 폰트가 적용될 수 있도록 해준다.
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}><App /></MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
